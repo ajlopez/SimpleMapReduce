8e3aa8712a9a3c90f41827ca95427b179a2b3c91
ajlopez <webmaster@ajlopez.com>
2013-02-02 21:09:38 -0300

    Samples refactor

--- samples/wordssync/run.js
+++ samples/wordssync/run.js
@@ -3,9 +3,9 @@ var simplemapreduce = require('../..');
 
 var result = simplemapreduce.runSync(
     ["A", "word", "is", "a", "word"], 
-    function (item) { return item.toLowerCase(); },
-    function (item, key) { return { count: 0 }; },
-    function (item, result) { result.count++; }
+    function (key, value, context) { context.emit(value.toLowerCase(), 1); },
+    function (key) { return { count: 0 }; },
+    function (key, value, result) { result.count += value; }
 );
 
 console.dir(result);
